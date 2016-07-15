fd7d07af739fdce832ba1c560cc89517074ccf04
Thuan G Pham <thuangpham@gmail.com>
2016-07-15 18:54:39 -0400

    Re upload to fix the order of the plot 4

--- Program/Plot4.R
+++ Program/Plot4.R
@@ -8,7 +8,7 @@ source("Program/LoadData.R")
 df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
 
 #plot to png
-#png("plot4.png",width=500,height=500)
+png("plot4.png",width=500,height=500)
 
 par(mfrow=c(2,2))
 
@@ -16,6 +16,9 @@ par(mfrow=c(2,2))
 plot(df$DateTime,df$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=NA)
 
 #top right
+plot(df$DateTime,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
+#bottom left 
+
 plot(df$DateTime, df$Sub_metering_1,ylab="Energy sub metering",xlab=NA,type="l",col="black")
 
 #add lines for sub_metering_2 column using red
@@ -24,9 +27,6 @@ lines(df$DateTime, df$Sub_metering_2, col="red")
 lines(df$DateTime, df$Sub_metering_3, col="blue")
 # add legend
 legend("topright", col = c("black", "red", "blue"), lty=1, legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-
-#bottom left 
-plot(df$DateTime,df$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
 #bottom right
 plot(df$DateTime,df$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
