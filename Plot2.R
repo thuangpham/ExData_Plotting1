b17f4913df0d0b81eb509a96bc763b34fe79ed5b
Thuan G Pham <thuangpham@gmail.com>
2016-07-16 06:57:12 -0400

    Move the read data code back to this program

--- Plot2.R
+++ Plot2.R
@@ -1,8 +1,16 @@
 #set the working directory
 setwd("C:/DataScience/Exploratory Data/ExData_Plotting1/ExData_Plotting1")
 
-##Read data using a function I created, since that code is being use for all the other program
-source("Program/LoadData.R")
+library(dplyr)
+file<-"household_power_consumption.txt"
+#read in only the data starting from the first row of the 2007-02-01 to 2007-02-02
+df <- read.csv(file, sep=";", header=TRUE,stringsAsFactors=FALSE,
+               colClasses = c("Date","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric"),
+               na.strings="?",skip = 66637, nrow = 2880)
+
+##apply the header row to the data
+name <- sapply(read.table(file, nrow = 1, sep = ";"), as.character)
+names(df) <- name
 
 ##set datetime column by combining Date and Time columns
 df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
