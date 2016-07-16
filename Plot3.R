7cb1ea00a8941c6daac205eb0c52f054dedb7a39
Thuan G Pham <thuangpham@gmail.com>
2016-07-16 07:01:28 -0400

    include load data function in this program

--- Plot3.R
+++ Plot3.R
@@ -1,8 +1,13 @@
 #set the working directory
 setwd("C:/DataScience/Exploratory Data/ExData_Plotting1/ExData_Plotting1")
 
-##Read data using a function I created, since that code is being use for all the other program
-source("Program/LoadData.R")
+library(dplyr)
+file<-"Data/household_power_consumption.txt"
+
+df <- read.csv(file, sep=";", header=TRUE,stringsAsFactors=FALSE,skip = 66637, nrow = 2880,
+               na.strings="?")
+name <- sapply(read.table(file, nrow = 1, sep = ";"), as.character)
+names(df) <- name
 
 ##set datetime column by combining Date and Time columns
 df$DateTime <- strptime(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
